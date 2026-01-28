4df712ef6ac2d6623ab88991e80d639cc761cefc
Paul Buckley <paul@hubble.com>
2026-01-28 07:34:39 -0800

    Update formula with release SHA256

--- Formula/mdsee.rb
+++ Formula/mdsee.rb
@@ -2,7 +2,7 @@ class Mdsee < Formula
   desc "Simple markdown file viewer for macOS with live reload"
   homepage "https://github.com/buckleypaul/mdsee"
   url "https://github.com/buckleypaul/mdsee/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256_FROM_STEP_2"
+  sha256 "9ee48d09b1622595b7236c63c0e4b66b3cbce9444b5f92698f4877976ed99552"
   license "MIT"
 
   depends_on :macos
